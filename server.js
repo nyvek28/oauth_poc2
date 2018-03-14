dbf6c4828fc0cf10256d6b293e68f940c3d06cb3
Kevyn Quiros <kevyn@ip-192-168-12-74.ec2.internal>
2018-03-14 13:36:13 -0600

    Change public to dist

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 var port = 3004;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 var server = app.listen(port, function() {
   console.log('Magic happens on port ' + port);
