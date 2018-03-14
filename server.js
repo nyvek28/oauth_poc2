ef208d2e1d1cd0e82d6334c3dd917456215c92e0
Kevyn Quiros <kevyn@ip-192-168-12-74.ec2.internal>
2018-03-14 14:33:02 -0600

    Add dynamic port to server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = 3004;
+var port = process.env.PORT || 3004;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
